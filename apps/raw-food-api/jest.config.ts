6859ef34c3ffd4b7e4bb918491f46860c3157b2e
Rawan <rawancheaito80@gmail.com>
2023-12-12 21:35:52 +0200

    Added : empty nestJs project

---
+++ apps/raw-food-api/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'raw-food-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/raw-food-api',
+};
