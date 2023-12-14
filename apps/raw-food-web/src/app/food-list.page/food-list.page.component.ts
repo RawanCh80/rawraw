2b8f3734af06deb59022d5ea019289d8d19e23ac
Rawan <rawancheaito80@gmail.com>
2023-12-14 20:32:58 +0200

    Created:raw-food-web project created:food-list-page

---
+++ apps/raw-food-web/src/app/food-list.page/food-list.page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'rawraw-food-list.page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './food-list.page.component.html',
+  styleUrl: './food-list.page.component.scss',
+})
+export class FoodListPageComponent {}
