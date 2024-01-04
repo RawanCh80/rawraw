a94e708f25cb2d869c1ad8c2c63fbd98f684dd9f
Rawan <rawancheaito80@gmail.com>
2024-01-04 16:50:12 +0200

    created:food details dialog

---
+++ apps/raw-food-web/src/app/food-list/food-details-dialog/food-details-dialog.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'rawraw-food-details-dialog',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './food-details-dialog.html',
+  styleUrl: './food-details-dialog.css',
+})
+export class FoodDetailsDialog {}
