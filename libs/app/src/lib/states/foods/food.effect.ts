51d9923b3a878723a771ea13dee22dcb5ee42e8b
Rawan <rawancheaito80@gmail.com>
2024-03-21 01:54:32 +0200

    Enhanced food effect with update, delete, and details actions
    The food effect file has been significantly expanded by the addition of the actions for updating food, deleting food, and loading food details. They are all handled with RxJS operators and hooked into the FoodsService. The error handling for these actions has also been implemented. Additionally, there were minor cosmetic changes in the existing loadFoods action.

--- libs/app/src/lib/states/foods/food.effect.ts
+++ libs/app/src/lib/states/foods/food.effect.ts
@@ -1,26 +1,113 @@
 import { Injectable } from '@angular/core';
 import { FoodActions, FoodItemBo, FoodsService } from '@rawraw/app';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, exhaustMap, map, of } from 'rxjs';
+import { catchError, exhaustMap, map, of, switchMap } from 'rxjs';
 
 @Injectable()
 export class FoodEffect {
+
+  public $createFood = createEffect(() =>
+    this.actions$
+      .pipe(
+        ofType(FoodActions.createFood),
+        exhaustMap((action) => {
+            return this.foodsService
+              .createFood(
+                {
+                  label: action.food.label,
+                  description: action.food.description
+                }
+              )
+              .pipe(
+                switchMap((food: FoodItemBo) => {
+                  return [FoodActions.createFoodSuccess(), FoodActions.loadFoods()];
+                }),
+                catchError((error) => {
+                    return of(FoodActions.createFoodFailure({errorMessage: 'Fail to load Food Details'}))
+                  }
+                )
+              );
+          }
+        )
+      )
+  );
   public $loadFoods = createEffect(() =>
     this.actions$
       .pipe(
         ofType(FoodActions.loadFoods),
         exhaustMap(() => {
-          console.log('le effect call');
-          return this.foodsService
+            console.log('le effect call');
+            return this.foodsService
               .getFoods()
               .pipe(
                 map((foodList: FoodItemBo[]) => {
                   console.log('leeeeeeeee');
-                  return FoodActions.loadFoodsSuccess({ foods: foodList });
+                  return FoodActions.loadFoodsSuccess({foods: foodList});
                 }),
                 catchError((error) => {
                     console.log('leeeeeeeee');
-                   return  of(FoodActions.loadFoodsFailure({ errorMessage: 'Fail to load Foods' }))
+                    return of(FoodActions.loadFoodsFailure({errorMessage: 'Fail to load Foods'}))
+                  }
+                )
+              );
+          }
+        )
+      )
+  );
+  public $loadFoodDetails = createEffect(() =>
+    this.actions$
+      .pipe(
+        ofType(FoodActions.loadFoodDetails),
+        exhaustMap((action) => {
+            return this.foodsService
+              .getFood(action.foodId)
+              .pipe(
+                map((food: FoodItemBo) => {
+                  return FoodActions.loadFoodDetailsSuccess({food});
+                }),
+                catchError((error) => {
+                    return of(FoodActions.loadFoodDetailsFailure({errorMessage: 'Fail to load Food Details'}))
+                  }
+                )
+              );
+          }
+        )
+      )
+  );
+  public $updateFood = createEffect(() =>
+    this.actions$
+      .pipe(
+        ofType(FoodActions.updateFood),
+        exhaustMap((action) => {
+            return this.foodsService
+              .updateFood(action.foodId, action.foodFormDetailsValue)
+              .pipe(
+                switchMap((food: FoodItemBo) => {
+                  return [FoodActions.updateFoodSuccess(), FoodActions.loadFoods()]
+                }),
+                catchError((error) => {
+                    return of(FoodActions.deleteFoodFailure({errorMessage: 'Fail to update Food'}))
+                  }
+                )
+              )
+          }
+        )
+      )
+  );
+
+  public $deleteFood = createEffect(() =>
+    this.actions$
+      .pipe(
+        ofType(FoodActions.deleteFood),
+        exhaustMap((action) => {
+            return this.foodsService
+              .deleteFood(action.foodId)
+              .pipe(
+                switchMap((food: FoodItemBo) => {
+                  return [FoodActions.deleteFoodSuccess(), FoodActions.loadFoods()];
+                }),
+                catchError((error) => {
+                    return of(FoodActions.deleteFoodFailure({errorMessage: 'Fail to delete Food'}))
                   }
                 )
               );
