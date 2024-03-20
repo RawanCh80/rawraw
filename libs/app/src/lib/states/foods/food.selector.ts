0f828b106f0a943dada43c74d7b443baa985f3f0
Rawan <rawancheaito80@gmail.com>
2024-03-21 01:53:11 +0200

    Add FoodDetails selector to food.selector.ts
    Selectors for FoodDetails have been added to the food.selector.ts file. This development enables the selection of state data from food details within the larger application state. This change aims to simplify the data selection process and improve state organization and management.

--- libs/app/src/lib/states/foods/food.selector.ts
+++ libs/app/src/lib/states/foods/food.selector.ts
@@ -1,5 +1,8 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { FOOD_KEY, FoodState } from './food.reducers';
+import { FOOD_DETAILS_KEY, FoodDetailsState } from "./food-details.reducer";
 
 export const selectFoodFeature = createFeatureSelector<FoodState>(FOOD_KEY);
+export const selectFoodDetailsFeature = createFeatureSelector<FoodDetailsState>(FOOD_DETAILS_KEY);
 export const selectAllFoods = createSelector(selectFoodFeature, (state: FoodState) => state);
+export const selectFoodDetails = createSelector(selectFoodDetailsFeature, (state: FoodDetailsState) => state);
