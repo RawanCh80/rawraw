ba41e595e0ef41fe3ce54f52ce42b8b6c29d5c1b
Rawan <rawancheaito80@gmail.com>
2024-03-21 01:57:46 +0200

    Add FoodDetailsStatusEnum
    A new enum file, FoodDetailsStatusEnum has been introduced for handling various food-related actions such as loading, creating, updating, and deleting. This file serves as a centralized location to manage all status codes for the food details functionality. The enum contains both success and error status codes for each action.

---
+++ libs/app/src/lib/states/enums/food-details-status.enum.ts
@@ -0,0 +1,12 @@
+export enum FoodDetailsStatusEnum {
+  pending = 'pending',
+  loading = 'loading',
+  createSuccess = 'createSuccess',
+  loadSuccess = 'loadSuccess',
+  updateSuccess = 'updateSuccess',
+  deleteSuccess = 'deleteSuccess',
+  createError = 'createError',
+  deleteError = 'deleteError',
+  updateError = 'updateError',
+  loadError = 'loadError',
+}
