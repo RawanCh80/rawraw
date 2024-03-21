e6ec572fe73cb2d201bf9bdb562b777744f41c2b
Rawan <rawancheaito80@gmail.com>
2024-03-21 02:05:12 +0200

    Implement CRUD actions in FoodActions using NgRx
    The FoodActions module has been enhanced to include create, read, update, and delete (CRUD) actions for managing state of Food objects. The newly introduced actions use NgRx props to capture necessary data, improving data flow within the application and enhancing error handling capabilities.

--- libs/app/src/lib/states/foods/food.action.ts
+++ libs/app/src/lib/states/foods/food.action.ts
@@ -1,11 +1,26 @@
 import { createActionGroup, emptyProps, props } from '@ngrx/store';
 import { FoodItemBo } from '../../foods/bos/food-item.bo';
+import { FoodForUpdatedDto } from "../../foods/dto/food-for-updated.dto";
+import { FoodForUpdateInterface } from "../../foods/interfaces/food-for-updated.interface";
+import { FoodForCreationInterface } from "../../foods/interfaces/food-for-creation.interface";
 
 export const FoodActions = createActionGroup({
   source: 'Foods',
   events: {
+    'create Food': props<{ food: FoodForCreationInterface }>(),
+    'create Food Success': emptyProps(),
+    'create Food Failure': props<{ errorMessage: string }>(),
+    'load Food Details': props<{ foodId: string }>(),
+    'load Food Details Success': props<{ food: FoodItemBo }>(),
+    'load Food Details Failure': props<{ errorMessage: string }>(),
     'load Foods': emptyProps(),
     'load Foods Success': props<{ foods: FoodItemBo[] }>(),
-    'load Foods Failure': props<{ errorMessage: string }>()
+    'load Foods Failure': props<{ errorMessage: string }>(),
+    'update Food': props<{ foodId: string, foodFormDetailsValue: FoodForUpdateInterface }>(),
+    'update Food Success': emptyProps(),
+    'update Food Failure': props<{ errorMessage: string }>(),
+    'delete Food': props<{ foodId: string }>(),
+    'delete Food Success': emptyProps(),
+    'delete Food Failure': props<{ errorMessage: string }>(),
   }
 });
