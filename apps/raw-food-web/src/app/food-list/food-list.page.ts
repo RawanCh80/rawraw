d64c9d4445dddc5ec02374c00a0b6d56e0e3f97d
Rawan <rawancheaito80@gmail.com>
2024-03-22 03:32:41 +0200

    Refactor food-list module to simplify dialog actions and add lifecycle hook
    The implemented changes include adding import statements for extra components and libraries. The dialog process has been refactored to remove redundancy and the AfterViewInit lifecycle hook has been introduced to the food-list module. This simplifies the dialog presentation and possibly enhances the page initialization process.

--- apps/raw-food-web/src/app/food-list/food-list.page.ts
+++ apps/raw-food-web/src/app/food-list/food-list.page.ts
@@ -1,18 +1,30 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FoodActions, FoodItemBo, FoodListBase } from '@rawraw/app';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { FOOD_KEY, FoodActions, FoodItemBo, FoodListBase, HttpStatusEnum, selectAllFoods } from '@rawraw/app';
 import { MatDialog } from '@angular/material/dialog';
 import { FoodCreateDialog } from './food-create-dialog/food-create-dialog';
 import { FoodDetailsDialog } from './food-details-dialog/food-details-dialog';
 import { FoodDeleteAlertDialog } from './food-delete-alert-dialog/food-delete-alert-dialog';
 import { lastValueFrom } from 'rxjs';
-import { Store } from "@ngrx/store";
+import { select, Store } from "@ngrx/store";
+import { LetDirective } from "@ngrx/component";
+import { MatButton } from "@angular/material/button";
+import { CommonModule } from '@angular/common';
+import { IonicModule } from "@ionic/angular";
 
 @Component({
   templateUrl: 'food-list.page.html',
-  styleUrls: ['food-list.page.scss']
+  styleUrls: ['food-list.page.scss'],
+  imports: [
+    CommonModule,
+    LetDirective,
+    MatButton,
+    IonicModule
+  ],
+  standalone: true
 })
-export class FoodListPage extends FoodListBase implements OnInit, OnDestroy {
-  private isDialogOpen = false;
+export class FoodListPage extends FoodListBase implements OnInit, OnDestroy,AfterViewInit {
+
+  public foodListSelected$ = this.store.pipe(select(selectAllFoods));
 
   constructor(
     private store: Store,
@@ -27,54 +39,47 @@ export class FoodListPage extends FoodListBase implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.subscription$.unsubscribe();
   }
+ngAfterViewInit(){
 
+}
   public async presentAlertDeleteFoodDialog(foodId: string) {
-    if (!this.isDialogOpen) {
-      const dialogRef = this.matDialog
-        .open(FoodDeleteAlertDialog,
-          {
-            data: {
-              foodId: foodId
-            },
-            height: '300px',
-            width: '1000px',
-            hasBackdrop: true
-          });
-      this.isDialogOpen = true;
-      await lastValueFrom(dialogRef.afterClosed());
-      this.isDialogOpen = false;
-    }
+    const dialogRef = this.matDialog
+      .open(FoodDeleteAlertDialog,
+        {
+          data: {
+            foodId: foodId
+          },
+          height: '158px',
+          width: '1000px',
+          hasBackdrop: true
+        });
+    await lastValueFrom(dialogRef.afterClosed());
   }
 
   public async presentAddFoodDialog() {
     const dialogRef = this.matDialog
       .open(FoodCreateDialog, {
-        height: '60vh',
-        width: '60vh',
+        height: '400px',
+        width: '500px',
         hasBackdrop: true
       });
     await lastValueFrom(dialogRef.afterClosed());
-    this.isDialogOpen = false;
   }
 
   public async presentFoodDetailsDialog(foodItemBo: FoodItemBo) {
-    if (!this.isDialogOpen) {
-      this.isDialogOpen = true;
-      const dialogRef = this.matDialog
-        .open(FoodDetailsDialog,
-          {
-            data: {
-              foodId: foodItemBo.id
-            },
-            height: '60vh',
-            width: '60vh',
-            position: {
-              top: '-80vh',
-              left: '30vh'
-            }
-          });
-      await lastValueFrom(dialogRef.afterClosed());
-      this.isDialogOpen = false;
-    }
+    const dialogRef = this.matDialog
+      .open(FoodDetailsDialog,
+        {
+          data: {
+            food: foodItemBo
+          },
+          height: '500px',
+          width: '500px',
+          hasBackdrop: true
+        });
+    await lastValueFrom(dialogRef.afterClosed());
   }
+
+  protected readonly FOOD_KEY = FOOD_KEY;
+  protected readonly HttpStatusEnum = HttpStatusEnum;
 }
