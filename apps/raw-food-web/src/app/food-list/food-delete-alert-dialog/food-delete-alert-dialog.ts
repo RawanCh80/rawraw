cdbc43b6fa4845b515e33df4db223f5a7555ef89
Rawan <rawancheaito80@gmail.com>
2024-03-22 03:33:52 +0200

    Refactor FoodDeleteAlertDialog to use NgRx store
    The FoodDeleteAlertDialog class has been refactored to use NgRx store instead of directly calling the foods service. The implementation now subscribes to the store to receive data about food operations such as delete success or delete error which are then used to decide the snack bar message. Additionally, lifecycle methods ngOnInit and ngOnDestroy have been added for subscription management.

--- apps/raw-food-web/src/app/food-list/food-delete-alert-dialog/food-delete-alert-dialog.ts
+++ apps/raw-food-web/src/app/food-list/food-delete-alert-dialog/food-delete-alert-dialog.ts
@@ -1,37 +1,62 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog } from "@angular/material/dialog";
-import { FoodsService } from "@rawraw/app";
-import { lastValueFrom } from "rxjs";
+import { Subscription } from "rxjs";
 import { MatSnackBar } from "@angular/material/snack-bar";
+import { select, Store } from "@ngrx/store";
+import { FoodActions, FoodDetailsStatusEnum, selectFoodDetails } from "@rawraw/app";
 
 @Component({
   templateUrl: './food-delete-alert-dialog.html',
   styleUrl: './food-delete-alert-dialog.scss',
 })
-export class FoodDeleteAlertDialog {
+export class FoodDeleteAlertDialog implements OnInit, OnDestroy {
+
+  private subscription$ = new Subscription();
+  public foodDetailsSelected$ = this.store.pipe(select(selectFoodDetails));
 
   constructor(public matDialog: MatDialog,
               public matSnackBar: MatSnackBar,
-              private foodService: FoodsService,
+              protected store: Store,
               @Inject(MAT_DIALOG_DATA) public data: { foodId: string }) {
   }
+
+  ngOnInit(): void {
+    this.foodSelectorSubscription();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription$.unsubscribe();
+  }
+
 
   public async dismissDeleteAlertDialog() {
     return this.matDialog.closeAll();
   }
 
-  async deleteFood() {
-    try {
-      await lastValueFrom(this.foodService
-        .deleteFood(this.data.foodId));
-      this.matSnackBar.open('food deleted successfully',
-        'Close', {
-          duration: 2000
-        });
-      await this.dismissDeleteAlertDialog();
-    } catch (err) {
-      this.matSnackBar.open('food cannot be deletedK');
-      console.error('');
-    }
+  private foodSelectorSubscription() {
+    this.subscription$.add(
+      this.foodDetailsSelected$.subscribe({
+          next: async (foodDetailsState) => {
+            if (foodDetailsState.status === FoodDetailsStatusEnum.deleteSuccess) {
+              this.matSnackBar.open('food deleted successfully',
+                'Close', {
+                  duration: 2000
+                });
+              await this.dismissDeleteAlertDialog();
+            }
+            if (foodDetailsState.status === FoodDetailsStatusEnum.deleteError) {
+              this.matSnackBar.open('food cannot be deleted');
+            }
+          }
+        }
+      )
+    )
+  }
+
+  async dispatchDeleteFood() {
+    this.store.dispatch(FoodActions.deleteFood({
+        foodId: this.data.foodId
+      })
+    );
   }
 }
