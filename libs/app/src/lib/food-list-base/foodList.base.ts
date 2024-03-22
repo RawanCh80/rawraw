3178976e831c8f4892a0272aeda9723bff08cf8d
Rawan <rawancheaito80@gmail.com>
2024-03-22 03:30:00 +0200

    Remove unnecessary imports in FoodListBase
    The commit removes unneeded imports in the FoodListBase file. The select, Store from '@ngrx/store', selectAllFoods from '../states/foods/food.selector', and inject from '@angular/core were eliminated

--- libs/app/src/lib/food-list-base/foodList.base.ts
+++ libs/app/src/lib/food-list-base/foodList.base.ts
@@ -1,7 +1,4 @@
 import { Subscription } from 'rxjs';
-import { select, Store } from '@ngrx/store';
-import { selectAllFoods } from '../states/foods/food.selector';
-import { inject } from '@angular/core';
 
 export abstract class FoodListBase {
   protected subscription$ = new Subscription();
