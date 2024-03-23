883b56bd1c82978b1482e5afbdfaf4deafa4a1ea
Rawan <rawancheaito80@gmail.com>
2024-03-24 01:29:39 +0200

    Add additional modal methods and parameters to FoodListBase
    Several modal display methods have been added to the abstract base class `FoodListBase`, creating a more streamlined and consistent interface. The `isEditMode` attribute has been added to distinguish between different modal displays. The `FOOD_KEY` and `HttpStatusEnum` have also been incorporated into the class for more effective utilization. The changes enhance the code's scalability and reusability.

--- libs/app/src/lib/food-list-base/foodList.base.ts
+++ libs/app/src/lib/food-list-base/foodList.base.ts
@@ -1,5 +1,19 @@
 import { Subscription } from 'rxjs';
+import { FOOD_KEY, FoodItemBo, HttpStatusEnum } from '@rawraw/app';
 
 export abstract class FoodListBase {
+  protected isEditMode = false;
   protected subscription$ = new Subscription();
+
+  constructor() {
+  }
+
+  protected abstract presentAlertDeleteFood(foodId: string): Promise<void>;
+
+  protected abstract presentAddFoodModal(): Promise<void>;
+
+  protected abstract presentFoodDetailsModal(foodItemBo: FoodItemBo): Promise<void>;
+
+  protected readonly FOOD_KEY = FOOD_KEY;
+  protected readonly HttpStatusEnum = HttpStatusEnum;
 }
