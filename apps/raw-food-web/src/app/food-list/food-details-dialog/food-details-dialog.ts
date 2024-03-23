eb75c6926c0f0fa9037c7c515683178b0f10b989
Rawan <rawancheaito80@gmail.com>
2024-03-23 23:57:03 +0200

    Refactor FoodDetailsDialog to extend FoodDetailsBase
    The FoodDetailsDialog component has been streamlined by letting it extend the FoodDetailsBase class, allowing for the removal of redundant code and properties. The 'dismissDialog' method is also replaced with 'dismissModal' for enhanced consistency across components. Extraction of the food details from the app data rather than as a direct injection in the constructor has been implemented for a cleaner interface.

--- apps/raw-food-web/src/app/food-list/food-details-dialog/food-details-dialog.ts
+++ apps/raw-food-web/src/app/food-list/food-details-dialog/food-details-dialog.ts
@@ -1,69 +1,55 @@
 import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from "@angular/forms";
-import { Subscription } from "rxjs";
-import { FoodActions, FoodDetailsStatusEnum, FoodItemBo, selectFoodDetails } from "@rawraw/app";
+import { FormGroup } from "@angular/forms";
+import { FOOD_DETAILS_KEY, FoodActions, FoodDetailsBase, FoodDetailsStatusEnum } from "@rawraw/app";
 import { MAT_DIALOG_DATA, MatDialog } from "@angular/material/dialog";
 import { MatSnackBar } from "@angular/material/snack-bar";
-import { select, Store } from "@ngrx/store";
-
-interface FoodForUpdateFormGroupInterface {
-  label: FormControl<string>;
-  description: FormControl<string>;
-}
 
 @Component({
   templateUrl: './food-details-dialog.html',
   styleUrls: ['./food-details-dialog.scss'],
 })
-export class FoodDetailsDialog implements OnInit, OnDestroy {
+export class FoodDetailsDialog extends FoodDetailsBase implements OnInit, OnDestroy {
   public foodDetailsForm: FormGroup;
-  private subscription$ = new Subscription();
-  public foodDetailsSelected$ = this.store.pipe(select(selectFoodDetails));
 
   constructor(
     public matDialog: MatDialog,
     private matSnackBar: MatSnackBar,
-    protected store: Store,
-    @Inject(MAT_DIALOG_DATA) public data: { food: FoodItemBo }) {
-    this.foodDetailsForm = new FormGroup<FoodForUpdateFormGroupInterface>({
-        label: new FormControl(''),
-        description: new FormControl('')
-      }
-    );
+    @Inject(MAT_DIALOG_DATA) public data: { foodId: string }) {
+    super();
   }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.foodSelectorSubscription();
     this.store.dispatch(FoodActions.loadFoodDetails({
-        foodId: this.data.food.id
+        foodId: this.data.foodId
       })
     );
   }
 
-  ngOnDestroy(): void {
+  public ngOnDestroy(): void {
     this.subscription$.unsubscribe();
   }
 
-  public async dismissDialog() {
+  protected async dismissModal() {
     return this.matDialog.closeAll();
   }
 
-  async dispatchUpdateFood() {
+  protected async dispatchUpdateFood() {
     this.store.dispatch(FoodActions.updateFood({
-      foodId: this.data.food.id,
+      foodId: this.data.foodId,
       foodFormDetailsValue: this.foodDetailsForm.value
     }));
   }
 
-  private foodSelectorSubscription() {
+  protected foodSelectorSubscription() {
     this.subscription$.add(
       this.foodDetailsSelected$.subscribe({
         next: async (foodDetailsState) => {
           if (foodDetailsState.status === FoodDetailsStatusEnum.loadSuccess) {
             console.log('success')
             this.foodDetailsForm.patchValue({
-              label: this.data.food.label,
-              description: this.data.food.description
+              label: foodDetailsState[FOOD_DETAILS_KEY].label,
+              description: foodDetailsState[FOOD_DETAILS_KEY].description
             })
           }
           if (foodDetailsState.status === FoodDetailsStatusEnum.updateSuccess) {
@@ -71,7 +57,7 @@ export class FoodDetailsDialog implements OnInit, OnDestroy {
               'Close', {
                 duration: 2000
               });
-            await this.dismissDialog();
+            await this.dismissModal();
           }
           if (foodDetailsState.status === FoodDetailsStatusEnum.updateError) {
             this.matSnackBar.open('food cannot be updated');
