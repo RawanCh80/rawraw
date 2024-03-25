ce283970e0acbd92dd3b672531c96f6b983deb03
Rawan <rawancheaito80@gmail.com>
2024-03-25 02:25:58 +0200

    Refactor food-details-dialog component code
    Renamed the file from food-details-dialog.ts to food-details.dialog.ts and decluttered code base by removing unused imports, functionality and created simplified version of the component.

---
+++ apps/raw-food-web/src/app/food-list/food-details-dialog/food-details.dialog.ts
@@ -0,0 +1,39 @@
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { FoodActions, FoodDetailsBase } from "@rawraw/app";
+import { MAT_DIALOG_DATA, MatDialog } from "@angular/material/dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
+
+@Component({
+  templateUrl: './food-details.dialog.html',
+  styleUrls: ['./food-details.dialog.scss'],
+})
+export class FoodDetailsDialog extends FoodDetailsBase implements OnInit, OnDestroy {
+
+  constructor(
+    public matDialog: MatDialog,
+    private matSnackBar: MatSnackBar,
+    @Inject(MAT_DIALOG_DATA) public data: { foodId: string }) {
+    super();
+  }
+
+  public ngOnInit(): void {
+    this.foodId = this.data?.foodId;
+    this.loadPage();
+    this.foodSelectorSubscription();
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription$.unsubscribe();
+  }
+
+  protected presentToast(message: string, duration: number) {
+    this.matSnackBar.open(message, 'Close', {
+      duration: duration
+    });
+  }
+
+  protected async dismissModal() {
+    this.store.dispatch(FoodActions.resetFoodDetailsStatus());
+    return this.matDialog.closeAll();
+  }
+}
