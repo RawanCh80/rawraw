a20c12a076d7e81c6dcfac0cd6421767d8be3a63
Rawan <rawancheaito80@gmail.com>
2024-03-25 02:27:09 +0200

    Refactor food details base class for optimization
    This update includes optimizations to the 'foodDetails.base.ts' class for better application performance. Key changes include the addition of 'loadPage', 'dispatchUpdateFood', and 'dispatchCreateFood' methods, and the renaming of 'editMode' to 'isEditMode'. The changes result in an enhanced flow between creating and updating food details, improving efficiency and readability of the code.

--- libs/app/src/lib/food-details-base/foodDetails.base.ts
+++ libs/app/src/lib/food-details-base/foodDetails.base.ts
@@ -1,7 +1,13 @@
 import { Subscription } from "rxjs";
 import { FormControl, FormGroup } from "@angular/forms";
 import { select, Store } from "@ngrx/store";
-import { selectFoodDetails } from "@rawraw/app";
+import {
+  FOOD_DETAILS_KEY,
+  FoodActions,
+  FoodDetailsStatusEnum,
+  FoodForCreationInterface,
+  selectFoodDetails
+} from "@rawraw/app";
 import { inject } from "@angular/core";
 
 interface FoodFormGroupInterface {
@@ -14,7 +20,8 @@ export abstract class FoodDetailsBase {
   protected subscription$ = new Subscription();
   protected foodDetailsSelected$ = this.store.pipe(select(selectFoodDetails));
   protected foodDetailsForm: FormGroup;
-  protected editMode: boolean;
+  protected isEditMode: boolean;
+  protected foodId: string;
 
   protected constructor() {
     this.foodDetailsForm = new FormGroup<FoodFormGroupInterface>({
@@ -24,11 +31,65 @@ export abstract class FoodDetailsBase {
     );
   }
 
+  protected loadPage() {
+    this.isEditMode = !!this.foodId;
+    if (this.isEditMode) {
+      this.store.dispatch(FoodActions.loadFoodDetails({
+          foodId: this.foodId
+        })
+      );
+    }
+  }
+
+  protected dispatchUpdateFood() {
+    this.store.dispatch(FoodActions.updateFood({
+          foodId: this.foodId,
+          foodFormDetailsValue: this.foodDetailsForm.value
+        }
+      )
+    );
+  }
+
+  protected dispatchCreateFood() {
+    const foodFormValue = this.foodDetailsForm.value as FoodForCreationInterface;
+    this.store.dispatch(FoodActions.createFood({
+        food: foodFormValue
+      })
+    );
+  }
+
+  protected foodSelectorSubscription() {
+    this.subscription$.add(
+      this.foodDetailsSelected$.subscribe({
+          next: async (foodDetailsState) => {
+            if (foodDetailsState.status === FoodDetailsStatusEnum.loadSuccess) {
+              this.foodDetailsForm.patchValue({
+                label: foodDetailsState[FOOD_DETAILS_KEY].label,
+                description: foodDetailsState[FOOD_DETAILS_KEY].description
+              })
+            }
+            if (foodDetailsState.status === FoodDetailsStatusEnum.updateSuccess) {
+              this.presentToast('food updated successfully', 3000);
+              await this.dismissModal();
+            }
+            if (foodDetailsState.status === FoodDetailsStatusEnum.updateError) {
+              this.presentToast('food cannot be updated', 3000);
+            }
+            if (foodDetailsState.status === FoodDetailsStatusEnum.createSuccess) {
+              this.presentToast('food created successfully', 3000);
+              await this.dismissModal();
+            }
+            if (foodDetailsState.status === FoodDetailsStatusEnum.createError) {
+              this.presentToast('food cannot be created', 3000);
+            }
+          }
+        }
+      )
+    );
+  }
+
+  protected abstract presentToast(message: string, duration: number): Promise<void> | void;
+
   protected abstract dismissModal(): Promise<void> | Promise<boolean>;
 
-  // protected abstract dispatchUpdateFood(): Promise<void> | void;
-
-  // protected abstract  dispatchCreateFood(): Promise<void>;
-
-  protected abstract foodSelectorSubscription(): void;
 }
