3676a686950f45c3f4d95becb1aae209d116a512
Rawan <rawancheaito80@gmail.com>
2024-03-25 02:26:09 +0200

    Simplify FoodDetailsModal component and remove unused code
    Condensed the FoodDetailsModal component in the raw-food-mobile app by eliminating unused imports and superfluous functions. Additionally, the component has been refactored for clarity and efficiency.

--- apps/raw-food-mobile/src/app/food-list/food-details-modal/food-details.modal.ts
+++ apps/raw-food-mobile/src/app/food-list/food-details-modal/food-details.modal.ts
@@ -1,13 +1,6 @@
 import { Component, inject, Input, OnDestroy, OnInit } from '@angular/core';
 import { ModalController, ToastController } from "@ionic/angular";
-import {
-  FOOD_DETAILS_KEY,
-  FoodActions,
-  FoodDetailsBase,
-  FoodDetailsStatusEnum,
-  FoodForCreationInterface,
-  FoodItemBo
-} from "@rawraw/app";
+import { FoodActions, FoodDetailsBase } from "@rawraw/app";
 
 @Component({
   templateUrl: './food-details.modal.html',
@@ -15,8 +8,7 @@ import {
 })
 
 export class FoodDetailsModal extends FoodDetailsBase implements OnInit, OnDestroy {
-  @Input() food: FoodItemBo;
-  @Input() isEditMode: boolean;
+  @Input() data: { foodId: string };
   private modalController = inject(ModalController);
   private toastController = inject(ToastController);
 
@@ -25,92 +17,26 @@ export class FoodDetailsModal extends FoodDetailsBase implements OnInit, OnDestr
   }
 
   public ngOnInit(): void {
+    this.foodId = this.data?.foodId;
+    this.loadPage();
     this.foodSelectorSubscription();
-    if (this?.isEditMode) {
-      this.store.dispatch(FoodActions.loadFoodDetails({
-          foodId: this.food.id,
-        })
-      );
-    }
   }
 
   public ngOnDestroy(): void {
     this.subscription$.unsubscribe();
   }
 
-  protected async dismissModal() {
+  protected dismissModal() {
     this.store.dispatch(FoodActions.resetFoodDetailsStatus());
     return this.modalController.dismiss(null, 'cancel');
   }
 
-  protected dispatchUpdateFood() {
-    this.store.dispatch(FoodActions.updateFood({
-          foodId: this.food.id,
-          foodFormDetailsValue: this.foodDetailsForm.value
-        }
-      )
-    );
-  }
-
-  public async dispatchCreateFood() {
-    const foodFormValue = this.foodDetailsForm.value as FoodForCreationInterface;
-    this.store.dispatch(FoodActions.createFood({
-        food: foodFormValue
-      })
-    );
-  }
-
-  protected foodSelectorSubscription() {
-    this.subscription$.add(
-      this.foodDetailsSelected$.subscribe({
-          next: async (foodDetailsState) => {
-            if (foodDetailsState.status === FoodDetailsStatusEnum.loadSuccess) {
-              this.foodDetailsForm.patchValue({
-                label: foodDetailsState[FOOD_DETAILS_KEY].label,
-                description: foodDetailsState[FOOD_DETAILS_KEY].description
-              })
-            }
-            if (foodDetailsState.status === FoodDetailsStatusEnum.updateSuccess) {
-              const toast = await this.toastController
-                .create({
-                  message: 'food updated successfully',
-                  duration: 2000,
-                  position: 'top'
-                });
-              await toast.present();
-              await this.dismissModal();
-            }
-            if (foodDetailsState.status === FoodDetailsStatusEnum.updateError) {
-              const toast = await this.toastController
-                .create({
-                  message: 'food cant be updated',
-                  duration: 2000,
-                  position: 'top'
-                });
-              await toast.present();
-            }
-            if (foodDetailsState.status === FoodDetailsStatusEnum.createSuccess) {
-              const toast = await this.toastController
-                .create({
-                  message: 'food created successfully',
-                  duration: 2000,
-                  position: 'top'
-                })
-              await toast.present();
-              await this.dismissModal();
-            }
-            if (foodDetailsState.status === FoodDetailsStatusEnum.createError) {
-              const toast = await this.toastController
-                .create({
-                  message: 'food cannot be created ',
-                  duration: 2000,
-                  position: 'top'
-                })
-              await toast.present();
-            }
-          }
-        }
-      )
-    );
+  protected async presentToast(message: string, duration: number) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: duration,
+      position: 'top'
+    });
+    await toast.present();
   }
 }
